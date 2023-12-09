394fa3599c42d389f168730154327a8b5d285f19
Antoine Veuiller <aveuiller@gmail.com>
2023-12-09 00:02:21 +0000

    feat(adventOfCode): day 8

---
+++ adventOfCode/2023/08/__main__.py
@@ -0,0 +1,65 @@
+# https://adventofcode.com/2023/day/1
+import os, re
+from typing import List, Optional
+from functools import reduce
+
+def load(test_mode: bool = False, name_override: Optional[str] = None) -> List[str]:
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    input_file = 'input.txt'
+    if test_mode:
+        input_file = f"test_{input_file}"
+    if name_override:
+        input_file = name_override
+    with open(os.path.join(dirname, input_file)) as f:
+        return f.readlines()
+
+def _build_instructions(instructions: str) -> List[int]:
+    return list(map(int, instructions.split()[0].replace('L', '0').replace('R', '1')))
+
+
+def _build_nodes(directions: List[str]) -> dict[str, List[str]]:
+    node_regex = re.compile(r'(?P<origin>\w+)\s+=\s+\((?P<left>\w+)\s*,\s+(?P<right>\w+)\)')
+    nodes = {}
+    for node in directions:
+        match = re.match(node_regex, node)
+        if match:
+            nodes[match.group("origin")] = [match.group("left"), match.group("right")]
+    return nodes
+
+def main_01(directions: List[str]) -> int:
+    start = "AAA"
+    end = "ZZZ"
+    lr_instructions = _build_instructions(directions.pop(0))
+    nodes = _build_nodes(directions)
+
+    i = 0
+    current_node = start
+    while current_node != end:
+        current_node = nodes[current_node][lr_instructions[i % len(lr_instructions)]]
+        i += 1
+
+    return i
+
+
+def main_02(directions: List[str]) -> int:
+    start_suffix = "A"
+    end_suffix = "Z"
+    lr_instructions = _build_instructions(directions.pop(0))
+    nodes = _build_nodes(directions)
+
+    i = 0
+    current_nodes = [node for node in nodes.keys() if node.endswith(start_suffix)]
+    while not all([node.endswith(end_suffix) for node in current_nodes]):
+        for j in range(len(current_nodes)):
+            current_nodes[j] = nodes[current_nodes[j]][lr_instructions[i % len(lr_instructions)]]
+        i += 1
+
+    return i
+
+
+if __name__ == '__main__':
+    # test = True
+    test = False
+    print(f"First Response: {main_01(load(test))}")
+    print(f"Second Response: {main_02(load(test))}")
+    # print(f"Second Response: {main_02(load(test, name_override='test_input_2.txt'))}")
